6df964615f74dd9d4866ac46c48a3803b22b1c66
Caio Haruo <caio.haruo@hotmail.com>
2023-04-28 13:04:36 -0300

    add dynamic properties for components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css'],
 })
-export class BigCardComponent {}
+export class BigCardComponent {
+  @Input()
+  photoCoverUrl: string = '';
+
+  @Input()
+  publicationDate: string = '';
+
+  @Input()
+  title: string = '';
+
+  @Input()
+  description: string = '';
+}
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent {
+  @Input()
+  publicationDate: string = '';
 
+  @Input()
+  photoCoverUrl: string = '';
+
+  @Input()
+  description: string = '';
 }
